7ee4dabed5d70e509df851a0b6c1d1f9c156b332
Michael <michael.kononenko101@gmail.com>
2022-09-20 15:18:21 +0300

    update for first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,8 +5,9 @@ console.log(galleryItems);
 
 const gallety = document.querySelector(".gallery");
 
-gallety.innerHTML = galleryItems.reduce((result, { preview, original, description }) => {
-  return (result += `<div class="gallery__item">
+gallety.innerHTML = galleryItems.reduce(
+  (result, { preview, original, description }) =>
+    (result += `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -15,26 +16,27 @@ gallety.innerHTML = galleryItems.reduce((result, { preview, original, descriptio
       alt="${description}"
     />
   </a>
-</div>`);
-}, "");
+</div>`),
+  ""
+);
 
-const galleryLinks = document.querySelectorAll(".gallery__link");
-galleryLinks.forEach((link) => link.addEventListener("click", openModal));
+gallety.addEventListener("click", openModal);
 
 function openModal(event) {
   event.preventDefault();
   const itemElement = event.target;
-  const dataSource = itemElement.dataset.source;
+  if (itemElement.nodeName !== "IMG") {
+    return;
+  }
 
   const instance = basicLightbox.create(`
-        <img src="${dataSource}" width="1000" height="1000">
+        <img src="${itemElement.dataset.source}" width="800" height="600">
     `);
   instance.show();
 
-  const modal = document.querySelector(".basicLightbox");
   document.addEventListener("keydown", (event) => {
     if (event.key === "Escape") {
-      modal.classList.remove("basicLightbox--visible");
+      instance.close();
     }
   });
 }
